3334bc0607fe741a33a10bc3bedf15e117e54d5d
Lukasz Nojek <9399633+lukaszmn@users.noreply.github.com>
2019-08-21 12:41:37 +0200

    Added subtitle

--- helpers.js
+++ helpers.js
@@ -2,9 +2,13 @@ function hr() {
 	$('body').append('<hr/>');
 }
 
-function title(link, descr) {
-	const text = 'Source' + (descr || '');
-	const html = $(`<a href="${link}">${text}</a>`);
+function title(link, text) {
+	const html = $(`<a href="${link}">${text || link}</a>`);
+	$('body').append(html);
+}
+
+function subtitle(params) {
+	const html = $(`<p>${params}</p>`);
 	$('body').append(html);
 }
 
