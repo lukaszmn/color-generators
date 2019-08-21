d455c35a1055c035188a73aca24401f248ddb403
Lukasz Nojek <9399633+lukaszmn@users.noreply.github.com>
2019-08-21 21:13:58 +0200

    Removed deprecated functions

--- helpers.js
+++ helpers.js
@@ -1,22 +1,3 @@
-function hr() {
-	$('body').append('<hr/>');
-}
-
-function title(link, text) {
-	const html = $(`<a href="${link}">${text || link}</a>`);
-	$('body').append(html);
-}
-
-function subtitle(params) {
-	const html = $(`<p>${params}</p>`);
-	$('body').append(html);
-}
-
-function div(color) {
-	const html = $('<div>').css('background-color', color).attr('title', color);
-	$('body').append(html);
-}
-
 // {link, description, section[] }
 // section: { title, params, getColors(count, params), div }
 const generators = [];
