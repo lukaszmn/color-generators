0e49416eaf5e56784f7cc04ac7cc0fc2f553b774
Lukasz Nojek <9399633+lukaszmn@users.noreply.github.com>
2019-08-21 14:56:26 +0200

    Extracted 3rd generator, added parameters sections

---
+++ generators/03.js
@@ -0,0 +1,22 @@
+addGenerator('https://stackoverflow.com/a/7352887', 'With brightness control', ``,
+(count, addColor) => {
+
+	function getRandColor(brightness) {
+		// Six levels of brightness from 0 to 5, 0 being the darkest
+		var rgb = [Math.random() * 256, Math.random() * 256, Math.random() * 256];
+		var mix = [brightness*51, brightness*51, brightness*51]; //51 => 255/5
+		var mixedrgb = [rgb[0] + mix[0], rgb[1] + mix[1], rgb[2] + mix[2]].map(function(x) { return Math.round(x/2.0)})
+		return "rgb(" + mixedrgb.join(",") + ")";
+	}
+	for (let br = 0; br < 6; ++br) {
+		subtitle('brightness: ' + br);
+		for (let i = 0; i < count; ++i) {
+			const color = getRandColor(br);
+			addColor(color);
+		}
+	}
+});
+
+/*
+addSection([br], params => )
+*/
